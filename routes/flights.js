deaad4fb4d9dd6e943d161d8da3e39c67c6dd251
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-02-28 19:49:28 -0500

    add create and view ticket functionality

--- routes/flights.js
+++ routes/flights.js
@@ -4,11 +4,13 @@ import * as flightsCtrl from '../controllers/flights.js'
 
 // listens for a get request on homepage to execute index controller function
 router.get('/', flightsCtrl.index)
-router.get('/:id', flightsCtrl.show)
+
 // listens for a get request on the /new page
 router.get('/new', flightsCtrl.new)
-
+router.get('/:id', flightsCtrl.show)
+router.post('/:id/tickets', flightsCtrl.createTicket)
 router.post('/', flightsCtrl.create)
+
 
 export {
   router
